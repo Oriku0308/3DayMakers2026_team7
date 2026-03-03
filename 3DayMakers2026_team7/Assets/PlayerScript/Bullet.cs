e4abdc114f751fb4eb11805f54efccc95b9d8937
rinrin4470 <rinrin4470@gmail.com>
2026-03-03 17:41:10 +0900

    弾の軌道や反射。当たった時のDebug管理

--- 3DayMakers2026_team7/Assets/PlayerScript/Bullet.cs
+++ 3DayMakers2026_team7/Assets/PlayerScript/Bullet.cs
@@ -3,8 +3,54 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private float speed = 10f;
+    [SerializeField] private float leftLimitx = -10f;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0f;
+        rb.freezeRotation = true;
+        rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+    }
+    private void Start()
+    {
+        rb.linearVelocity = transform.right * speed;
+    }
     void Update()
     {
-        transform.position += transform.right * speed * Time.deltaTime;
+        if (transform.position.x < leftLimitx)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("BadBoy"))
+        {
+            Debug.Log("BadBoy‚É“–‚½‚è‚Ü‚µ‚½");
+            Destroy(gameObject);
+            return;
+        }
+        else if (collision.gameObject.CompareTag("GoodBoy"))
+        {
+            Debug.Log("GoodBoy‚É“–‚½‚è‚Ü‚µ‚½");
+            Destroy(gameObject); 
+            return;
+        }
+        //if (collision.gameObject.CompareTag("Wall"))
+        //{
+        //    Conta
+        //    Vector2 normal = collision.contacts[0].normal;
+        //    Vector2 newDir = Vector2.Reflect(rb.linearVelocity.normalized, normal);
+        //    rb.linearVelocity = newDir * speed;
+        //}
+    }
+
+    public void Reflect(Vector2 normal)
+    {
+        Vector2 newDir = Vector2.Reflect(transform.right.normalized, normal);
+        rb.linearVelocity = newDir * speed;
+        transform.right = newDir;
     }
 }
