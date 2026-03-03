d38e2fd1a39ad32cef8ca888846a4310578ce136
KNTy1018 <soramametaro1018@gmail.com>
2026-03-03 12:55:57 +0900

    [add]エネミー群の生成処理

---
+++ 3DayMakers2026_team7/Assets/_Scripts/EnemyGroup.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+/// <summary>エネミー群のクラス</summary>
+public class EnemyGroup : MonoBehaviour
+{
+    //必要な処理が今のところない
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+}
---
+++ 3DayMakers2026_team7/Assets/_Scripts/EnemyGroupGenerator.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+/// <summary>エネミー群を生成するクラス</summary>
+public class EnemyGroupGenerator : MonoBehaviour
+{
+    [SerializeField,Tooltip("エネミー群\n戦闘要素がステージの初め")] private EnemyGroup[] _enemyGroups;
+
+    /// <summary>
+    /// 初期化関数
+    /// </summary>
+    [ContextMenu("Initialize")]
+    public void Initialize()
+    {
+        //初めにすべて非アクティブ
+        foreach (var enemyGroup in _enemyGroups)
+        {
+            enemyGroup.gameObject.SetActive(false);
+        }
+        //配列の先頭をアクティブ
+        _enemyGroups[0].gameObject.SetActive(true);
+    }
+
+    /// <summary>
+    /// エネミー群を生成する関数
+    /// </summary>
+    [ContextMenu("EnemyGroupGenerate")]
+    public void EnemyGroupGenerate()
+    {
+        //現在アクティブなエネミー群を非アクティブ
+        _enemyGroups[0].gameObject.SetActive(false);
+        Debug.Log($"No.{1} : Inactive");
+        //アクティブだったオブジェクト以外から抽選
+        var rand = Random.Range(1, _enemyGroups.Length);
+        //新旧の要素を入れ替え
+        (_enemyGroups[0], _enemyGroups[rand]) = (_enemyGroups[rand], _enemyGroups[0]);
+        //新しくオブジェクトをアクティブ
+        _enemyGroups[0].gameObject.SetActive(true);
+        Debug.Log($"No.{rand + 1} : Active");
+    }
+}
