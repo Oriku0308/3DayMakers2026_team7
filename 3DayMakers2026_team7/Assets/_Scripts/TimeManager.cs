cbc33b681ff041a8362fb3387c24c3334b43c045
flaming8634 <flaming.8634@icloud.com>
2026-03-04 14:43:24 +0900

    [Update]Eventを購読可能に変更

---
+++ 3DayMakers2026_team7/Assets/_Scripts/TimeManager.cs
@@ -0,0 +1,97 @@
+using UnityEngine;
+using TMPro;
+
+/// <summary>
+/// InGame中のTimerを管理するクラス
+/// </summary>
+
+public class TimeManager : MonoBehaviour
+{
+    public float CurrentTime { get; private set; }
+
+    [SerializeField] private TextMeshProUGUI _timeText;
+    [SerializeField] private float _maxTime = 90f;
+
+    private bool _isTimerRunning = false;
+
+    // 以下はpublicメゾット
+    public void TimerReset()
+    {
+        CurrentTime = _maxTime;
+        _isTimerRunning = false;
+        UpdateText();
+    }
+
+    // 以下はprivateメゾット
+
+    private void Start()
+    {
+        TimerReset();
+    }
+
+    private void Update()
+    {
+        if (!_isTimerRunning) return;
+
+        if (CurrentTime > 0)
+        {
+            CurrentTime -= Time.deltaTime;
+            UpdateText();
+
+            if(CurrentTime <= 0)
+            {
+                CurrentTime = 0;
+                UpdateText();
+                OnTimeUp();
+                return;
+            }
+        }
+    }
+
+    private void OnEnable()
+    {
+        EventHub.GameStartEvent += StartTimer;
+        EventHub.GameEndEvent += StopTimer;
+        EventHub.GamePauseEvent += StopTimer;
+        EventHub.GameResumedEvent += StartTimer;
+    }
+
+    private void OnDisable()
+    {
+        EventHub.GameStartEvent -= StartTimer;
+        EventHub.GameEndEvent -= StopTimer;
+        EventHub.GamePauseEvent -= StopTimer;
+        EventHub.GameResumedEvent -= StartTimer;
+    }
+
+    [ContextMenu("StartTimer")]
+    private void StartTimer()
+    {
+        _isTimerRunning = true;
+    }
+
+    [ContextMenu("StopTimer")]
+    private void StopTimer()
+    {
+        _isTimerRunning = false;
+    }
+
+    private void UpdateText()
+    {
+        // TimerのText表示を更新
+        if (_timeText != null)
+        {
+            _timeText.text = Mathf.CeilToInt(CurrentTime).ToString();
+        }
+    }
+
+    private void OnTimeUp()
+    {
+        // タイムアップ時の処理をここに記述
+        StopTimer();
+        SceneTransitionManager.TransitionToResult();
+        Debug.Log("Time's up!");
+
+        // EventHub.GameEndEvent?.Invoke();
+    }
+}
