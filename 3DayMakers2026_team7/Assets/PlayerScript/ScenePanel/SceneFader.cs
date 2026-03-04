dafb61c8dcbab820a4647be60eab73ea0d41b49a
rinrin4470 <rinrin4470@gmail.com>
2026-03-04 17:17:01 +0900

    [add]scene遷移時の暗転と明転の機能追加。

---
+++ 3DayMakers2026_team7/Assets/PlayerScript/ScenePanel/SceneFader.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneFader : MonoBehaviour
+{
+    [SerializeField] private Image fadeImage;
+    [SerializeField] private float fadeDuration = 2f;
+
+    [SerializeField] private bool playFadeInOnStart = true; // Å© ÉeÉXÉgóp
+
+    private void Start()
+    {
+        if (playFadeInOnStart)
+        {
+            StartCoroutine(FadeIn());
+        }
+    }
+
+    // ===== ñæì] =====
+    public IEnumerator FadeIn()
+    {
+        float time = 0f;
+
+        while (time < fadeDuration)
+        {
+            time += Time.deltaTime;
+            float alpha = 1 - (time / fadeDuration);
+            fadeImage.color = new Color(0, 0, 0, alpha);
+            yield return null;
+        }
+
+        fadeImage.color = new Color(0, 0, 0, 0);
+    }
+
+    // ===== à√ì]ÇµÇƒÉVÅ[ÉìëJà⁄ =====
+    public void FadeToScene(string sceneName)
+    {
+        StartCoroutine(FadeOutAndLoad(sceneName));
+    }
+
+    private IEnumerator FadeOutAndLoad(string sceneName)
+    {
+        float time = 0f;
+
+        while (time < fadeDuration)
+        {
+            time += Time.deltaTime;
+            float alpha = time / fadeDuration;
+            fadeImage.color = new Color(0, 0, 0, alpha);
+            yield return null;
+        }
+
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
