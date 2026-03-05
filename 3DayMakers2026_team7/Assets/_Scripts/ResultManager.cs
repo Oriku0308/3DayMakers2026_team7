d9dd2b522dca797c3202981cda256bcd3b31362b
Oriku0308 <O.riku8863@gmail.com>
2026-03-05 12:47:06 +0900

    [add]音素材の組み込み、一部素材のインポート

--- 3DayMakers2026_team7/Assets/_Scripts/ResultManager.cs
+++ 3DayMakers2026_team7/Assets/_Scripts/ResultManager.cs
@@ -4,14 +4,16 @@ using UnityEngine.UI;
 public class ResultManager : MonoBehaviour
 {
     [SerializeField] private Button _returnToTitleButton;
+    [SerializeField] private float _fadeDuration = 1f;
 
     void Start()
     {
+        SceneFader.FadeIn(_fadeDuration);
         _returnToTitleButton.onClick.AddListener(OnTitleButtonClick);
     }
 
     private void OnTitleButtonClick()
     {
-        SceneTransitionManager.TransitionToTitle();
+        // SceneTransitionManager.TransitionToTitle();
     }
 }
